5c0874a4226ac83a64483a9ace9d0e33b4c54139
Artur <mrartur0074@gmail.com>
2020-02-11 16:38:26 +0500

    Deploy

---
+++ precache-manifest.a810dd18ae7230ef40c4aeaceda1d511.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220488567a9048d5d4d0019a19779fda",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "88e3829c89cd9fff63fb",
+    "url": "/my-app/static/css/main.bd140f12.chunk.css"
+  },
+  {
+    "revision": "d32a703bd34f2ace1f7c",
+    "url": "/my-app/static/js/2.84afa341.chunk.js"
+  },
+  {
+    "revision": "88e3829c89cd9fff63fb",
+    "url": "/my-app/static/js/main.7aaa8ec0.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
